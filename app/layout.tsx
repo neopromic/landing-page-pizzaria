1f62eadb84d0f7e776eb9d58943192c5bcefe1d7
Weslley S. | Neo <cusaowesley1@gmail.com>
2024-08-12 10:52:55 -0300

    feat: add shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/Header";
+import { Pizza } from "lucide-react";
+import { NavLink } from "@/components/Header/Container/ActionContainer/NavLinks";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <Header.RootContainer>
+          <Header.LogoContainer.Logo>
+            <Pizza size={24} />
+          </Header.LogoContainer.Logo>
+          <Header.ActionContainer>
+            <NavLink title="RESERVA" url="#" />
+            <NavLink title="CARDÁPIO" url="#" />
+            <NavLink title="CONTATO" url="#" />
+          </Header.ActionContainer>
+        </Header.RootContainer>
+        {children}
+      </body>
     </html>
   );
 }
